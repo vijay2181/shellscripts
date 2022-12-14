2291dc4ab25b461e97d99984a459453aa5d2def8
vijay2181 <66196388+vijay2181@users.noreply.github.com>
2022-12-14 20:54:42 +0530

    Create aws_config_file.sh

---
+++ aws_config_file.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#USAGE: aws_config_file.sh [profile] [Access_key] [Secrect_key]
+
+# Fill your Access and secret keys here
+config () {
+   cat >> ~/.aws/config << EOF
+[profile $1]
+aws_access_key_id=$2
+aws_secret_access_key=$3
+region=us-west-2
+EOF
+}
+
+
+if [[ ! -e ~/.aws ]]; then
+    mkdir ~/.aws
+    touch ~/.aws/config
+    echo "Directory and config File are Created"
+    config $1 $2 $3
+elif [[ -d ~/.aws ]] && [[ -f ~/.aws/config ]]; then
+    echo "Directory and config File exists, so Appending Configs"
+    config $1 $2 $3
+fi
