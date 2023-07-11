f2b94d26f14cd396a9dfa48746251ae08b47b45c
Vijay Kumar Anuganti <66196388+vijay2181@users.noreply.github.com>
2023-07-11 12:23:22 +0530

    Create install-awscli2-ubuntu.sh

---
+++ install-awscli2-ubuntu.sh
@@ -0,0 +1,9 @@
+#Install awscli version2
+
+sudo apt update
+sudo apt upgrade -y
+sudo curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+sudo unzip awscliv2.zip
+sudo ./aws/install
+aws --version
