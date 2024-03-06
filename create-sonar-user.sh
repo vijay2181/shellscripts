0615b3488c0993b07943d4df53c4a69cbd28f6bf
Vijay Kumar <66196388+vijay2181@users.noreply.github.com>
2024-03-06 09:31:24 +0530

    Update create-sonar-user.sh

--- create-sonar-user.sh
+++ create-sonar-user.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 if [ "$(id -u)" -ne 0 ]; then
-        echo 'This script must be run by root' >&2
-        exit 1
+    echo 'This script must be run by root' >&2
+    exit 1
 fi
 
 echo "Please Enter the USER to create and add in sudoers file :"
 read RESULT
 
-if id $RESULT  >/dev/null 2>&1; then
-        echo "User $RESULT Already Exists, Skipping Now ...."
+if id "$RESULT" >/dev/null 2>&1; then
+    echo "User $RESULT Already Exists, Skipping Now ...."
 else
-        echo "User $RESULT  Does Not Exist, Creating Now ...."
-        groupadd $RESULT
-        useradd -g $RESULT -G admin -s /bin/bash -d /home/$RESULT $RESULT
-        mkdir -p /home/$RESULT
-        cp -r /root/.ssh /home/$RESULT/.ssh
-        chown -R $RESULT:$RESULT /home/$RESULT
-        echo "$RESULT    ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+    echo "User $RESULT Does Not Exist, Creating Now ...."
+    useradd -m -g $RESULT -s /bin/bash "$RESULT"
+    if [ $? -eq 0 ]; then
         echo "User $RESULT Created Successfully ...."
+        # Add user to sudoers
+        echo "$RESULT    ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+        echo "User $RESULT Added to sudoers file Successfully ...."
+    else
+        echo "Failed to create user $RESULT" >&2
+        exit 1
+    fi
 fi
+
